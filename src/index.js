2cbde90c9424077db31e6e937eb5a92130f72958
gauravAtProptiger <gaurav@proptiger.com>
2019-11-01 22:19:36 +0530

    dsfasfsdf

--- src/index.js
+++ src/index.js
@@ -11,10 +11,7 @@ import thunk from "redux-thunk";
 
 import events from "./reducers/";
 
-const composeEnhancers =
-  process.env.NODE_ENV === "development"
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const store = createStore(events, composeEnhancers(applyMiddleware(thunk)));
 
